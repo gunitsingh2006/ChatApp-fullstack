ef2197a12c1a4d7bda2256f4696c74d6d020651f
Gunit Singh <gunitsingh.duggal@gmail.com>
2026-08-08 17:57:57 +0530

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>App</div>
+function App(){
+  return(
+    <div className="bg-red-600 h-screen text-5xl">
+      <h1>Welcome to ChatApp</h1>
+    </div>
   )
 }
-
 export default App
