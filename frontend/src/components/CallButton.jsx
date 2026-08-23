4c0167a7c8c4dbb46b04218fc469b16d8aff5b05
Gunit Singh <gunitsingh.duggal@gmail.com>
2026-08-23 22:12:23 +0530

    chat

---
+++ frontend/src/components/CallButton.jsx
@@ -0,0 +1,13 @@
+import { VideoIcon } from "lucide-react";
+
+function CallButton({ handleVideoCall }) {
+  return (
+    <div className="p-3 border-b flex items-center justify-end max-w-7xl mx-auto w-full ">
+      <button onClick={handleVideoCall} className="btn btn-success btn-sm text-white rounded-4xl">
+        <VideoIcon className="size-6" />
+      </button>
+    </div>
+  );
+}
+
+export default CallButton;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-
+import "stream-chat-react/dist/css/v2/index.css";
+import "./index.css";
 //tanstack
 import {
   useQuery,
